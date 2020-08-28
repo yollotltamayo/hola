a505f9d7584daca1f16116871df12e4484e9dac3
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2020-08-28 16:56:41 -0500

    Updates

---
+++ precache-manifest.1c9d1bb88c9b2b6e4d3a679adcb88f50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa41d6bd10c083471678226a3ad3a270",
+    "url": "/hola/index.html"
+  },
+  {
+    "revision": "a421fdcf863ebfe962ce",
+    "url": "/hola/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "43ecf5d73d3c473ffebb",
+    "url": "/hola/static/css/main.7471df9f.chunk.css"
+  },
+  {
+    "revision": "a421fdcf863ebfe962ce",
+    "url": "/hola/static/js/2.072d9cc5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hola/static/js/2.072d9cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ecf5d73d3c473ffebb",
+    "url": "/hola/static/js/main.9b6faaed.chunk.js"
+  },
+  {
+    "revision": "f4ef784f343e09415741",
+    "url": "/hola/static/js/runtime-main.1c8da2ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hola/static/media/logo.5d5d9eef.svg"
+  }
+]);
